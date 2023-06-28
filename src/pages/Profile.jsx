76873d2c6d55b48669593784cb47c105396cd7a4
iGeek <jesus.cardenas.d@gmail.com>
2023-06-27 20:09:44 -0600

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,26 +1,49 @@
+import { useState } from "react";
 
 function Profile() {
+
+    const [formularioPerfil, setFormularioPerfil] = useState({
+        nombre: '',
+        email: '',
+        dob: '',
+        password: ''
+    });
+
+    const handleInputChange = (e) => {
+        // el evento de cambio en los input
+        setFormularioPerfil({
+            ...formularioPerfil,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const handleSubmit = (e) => {
+        // este es el evento de envio del formulario
+        e.preventDefault();
+        console.log(formularioPerfil);
+        // ...llamada a actualizar el perfil.....
+    }
 
 
     return (
         <div className="container mt-4">
-            <form className="form-login">
+            <form className="form-login" onSubmit={handleSubmit}>
                 <h1 className="text-center mb-5">Informacion de la cuenta</h1>
                 <div className="mb-3">
                     <label className="form-label">Nombre</label>
-                    <input type="text" className="form-control form-control-lg" id="name" name="name" />
+                    <input type="text" className="form-control form-control-lg" value={formularioPerfil.nombre} onChange={handleInputChange} name="nombre"/>
                 </div>
                 <div className="mb-3">
                     <label className="form-label">Email</label>
-                    <input type="email" className="form-control form-control-lg" id="last_name" name="last_name" />
+                    <input type="email" className="form-control form-control-lg" value={formularioPerfil.email} onChange={handleInputChange} name="email"/>
                 </div>
                 <div className="mb-3">
                     <label className="form-label">Fecha nacimiento</label>
-                    <input type="date" className="form-control form-control-lg" id="email" name="email" />
+                    <input type="date" className="form-control form-control-lg" value={formularioPerfil.dob} onChange={handleInputChange} name="dob"/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="password" className="form-label form-label-lg">Password</label>
-                    <input type="password" className="form-control form-control-lg" id="password" name="password" />
+                    <input type="password" className="form-control form-control-lg" value={formularioPerfil.password} onChange={handleInputChange} name="password"/>
                 </div>
                 <div className="d-grid">
                     <button className="btn btn-primary" type="submit">Guardar</button>
